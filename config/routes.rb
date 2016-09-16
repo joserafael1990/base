a4479b2bea0809888e43de1417331ac7ab9d2958
jose <joserafael1990@hotmail.com>
2016-09-16 15:54:48 -0700

    aplicacion base de registro de usuarios casi lista

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+	resources :categories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :path => '', :path_names => 
+  {:sign_in => 'login', :sign_out => 'logout',  :sign_up => 'signup', :edit => 'profile'}
+  root  "home#index"
+end
