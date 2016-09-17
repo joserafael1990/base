ab81ee31796c935ef8789038fddc84af90401452
jose <joserafael1990@hotmail.com>
2016-09-16 21:55:40 -0700

    login listo

--- app/controllers/no_admin_controller.rb
+++ app/controllers/no_admin_controller.rb
@@ -1,2 +1,7 @@
 class NoAdminController < ApplicationController
+	def index
+	end
+
+	def show
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
 	resources :categories
 	resources :admin
 	resources :no_admin
