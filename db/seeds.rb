5e4ce1db540b37f040219b19bc0fb357db123ddb
jose <joserafael1990@hotmail.com>
2016-09-18 23:46:47 -0700

    pulido con html decente

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+	
+	states = State.create([{name: 'Amazonas'},{name: 'Anzoátegui'},{name: 'Apure'},{name: 'Aragua'},{name: 'Barinas'},{name: 'Bolívar'},{name: 'Carabobo'},{name: 'Cojedes'},{name: 'Delta Amacuro'},{name: 'Falcón'},{name: 'Guárico'},{name: 'Lara'},{name: 'Mérida'},{name: 'Miranda'},{name: 'Monagas'},{name: 'Nueva Esparta'},{name: 'Portuguesa'},{name: 'Sucre'},{name: 'Trujillo'},{name: 'Vargas'},{name: 'Yaracuy'},{name: 'Zulia'}])
